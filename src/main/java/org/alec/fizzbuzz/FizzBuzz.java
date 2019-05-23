5f72de18f2665324b39bef5b30eefe17f2a6ab29
Joe.Hayward <joseph.hayward@autotrader.co.uk>
2019-05-23 13:47:32 +0100

    fixes made to fizzbuzz

--- src/main/java/org/alec/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/alec/fizzbuzz/FizzBuzz.java
@@ -1,17 +1,18 @@
 package org.alec.fizzbuzz;
 
-static import org.alec.words.Words.*;
+import static org.alec.words.Words.*;
 
 public class FizzBuzz {
 
-    public String play() {
-        int currentNumber = 1;
-        String result;
+    public String play(int number) {
+        int currentNumber = number;
+        String result = String.valueOf(currentNumber);
         int remainder = currentNumber % 15;
 
-        switch (remainder){
-            default:
-                result = currentNumber;
+        switch (remainder){ // this works for 1-14. 15, remainder 0.. prints 15 ()
+            case 0:
+              result = Words.FIZZ + Words.BUZZ;
+              break;
             case 3:
             case 6:
             case 9:
@@ -20,13 +21,20 @@ public class FizzBuzz {
                 break;
             case 5:
             case 10:
-                result = BUZZ;
+                result = Words.BUZZ;
                 break;
             case 15:
-                result = FIZZ + BUZZ;
+                result = Words.FIZZ + Words.BUZZ;
                 break;
         }
-        currentNumber.increment();
         return result;
     }
 }
+
+// line 3 - switched static import to import static
+// lines 24/27 - called BUZZ & FIZZ using class name
+// line 7 - added argument of int number
+// line 8 - assigned currentNumber as number, not 1 (maybe remove this variable)
+// removed currentNumber.increment()
+// Line 9 - assigned currentNumber to result variable
+// Line 13-15 - added case 0 for numbers divisible by 3 & 5
