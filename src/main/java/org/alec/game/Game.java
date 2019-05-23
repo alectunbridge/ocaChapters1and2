0dc368c54e9745a1c2cc24a6cbbd6a92f9e59190
Joe.Hayward <joseph.hayward@autotrader.co.uk>
2019-05-23 14:43:16 +0100

    fixed so number will start at 1 and end at 100

--- src/main/java/org/alec/game/Game.java
+++ src/main/java/org/alec/game/Game.java
@@ -3,17 +3,10 @@ package org.alec.game;
 public class Game {
     public static void main(String ... args) {
         FizzBuzz fizzBuzz = new FizzBuzz;
-        int i = 0;
+        int i = 1;
 
         do {
             System.out.println(fizzBuzz.play(i));
-        } while (++i<100);
+        } while (++i<=100);
     }
 }
-
-
-// moved variable i to class level
-// created new instance of fizzBuzz
-// removed continue statement
-// passed i in to play() method call
-// fixed Main method signature
